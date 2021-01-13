5bba8fe6612ea77f0a273ec5fa3f77b37570b132
熊同学 <oncwnuKQKOPq4vP2ptHVqKvGnzJw@git.weixin.qq.com>
2021-01-13 22:28:57 +0800

    增强查找

---
+++ WindowInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XtuHandleUitls
+{
+    public class WindowInfo
+    {
+        public IntPtr Hwnd;
+        public string WindowName;
+        public string ClassName;
+    }
+}
--- XtuHandle.cs
+++ XtuHandle.cs
@@ -17,8 +17,154 @@ namespace XtuHandleUitls
         public const int WM_GETTEXTLENGTH = 0x000E;//获取文本长度
 
 
+        /// <summary>
+        /// 获取在运行的所有窗口相关句柄信息
+        /// </summary>
+        /// <returns></returns>
+        public static List<WindowInfo> GetAllWindowList()
+        {
+            List<WindowInfo> windowInfoList = new List<WindowInfo>();
+            StringBuilder sb = new StringBuilder(4048);
+            EnumWindows((IntPtr hwnd, int lParam) =>
+            {
+                WindowInfo windowInfo = new WindowInfo();
+
+                windowInfo.Hwnd = hwnd;
+                GetWindowText(hwnd, sb, sb.Capacity);
+                windowInfo.WindowName = sb.ToString();
+                GetClassName(hwnd, sb, sb.Capacity);
+                windowInfo.ClassName = sb.ToString();
+
+                windowInfoList.Add(windowInfo);
+                return true;
+            }, 0);
+            return windowInfoList;
+        }
+
+        /// <summary>
+        /// 获取该窗口的所有子窗口句柄信息
+        /// </summary>
+        /// <param name="hwnd">父句柄</param>
+        /// <returns></returns>
+        public static List<ChildWindowInfo> GetAllChildWindowList(this WindowInfo wi)
+        {
+            List<ChildWindowInfo> windowInfoList = new List<ChildWindowInfo>();
+            StringBuilder sb = new StringBuilder(4048);
+
+            EnumChildWindows(wi.Hwnd, (IntPtr childHwnd, int lParam) =>
+            {
+                ChildWindowInfo windowInfo = new ChildWindowInfo();
+
+                windowInfo.Hwnd = childHwnd;
+                GetWindowText(childHwnd, sb, sb.Capacity);
+                windowInfo.WindowName = sb.ToString();
+                GetClassName(childHwnd, sb, sb.Capacity);
+                windowInfo.ClassName = sb.ToString();
+
+                windowInfoList.Add(windowInfo);
+                return true;
+            }, 0);
+            return windowInfoList;
+        }
+
+        /// <summary>
+        /// 根据参数查找获取某个运行窗口相关信息(文本类型支持模糊寻找)
+        /// </summary>
+        /// <param name="hwnd"></param>
+        /// <param name="windowName"></param>
+        /// <param name="className"></param>
+        /// <returns>返回找到的第一个</returns>
+        public static WindowInfo FindWindowInfo(IntPtr? hwnd, string windowName, string className)
+        {
+            List<WindowInfo> windowInfoList = GetAllWindowList();
+
+            if (hwnd != null && hwnd != (IntPtr)0)
+            {
+                if (windowInfoList.FindIndex(o => o.Hwnd == hwnd) != -1)
+                    return windowInfoList.Find(o => o.Hwnd == hwnd);
+            }
+            else if (windowName != null)
+            {
+                if (windowInfoList.FindIndex(o => o.WindowName.Contains(windowName)) != -1)
+                    return windowInfoList.Find(o => o.WindowName.Contains(windowName));
+            }
+            else if (className != null)
+            {
+                if (windowInfoList.FindIndex(o => o.ClassName.Contains(className)) != -1)
+                    return windowInfoList.Find(o => o.ClassName.Contains(className));
+            }
+            return new WindowInfo();
+        }
+
+        /// <summary>
+        /// 找到该父窗口下面的某个子窗口的相关信息（文本类型支持模糊寻找）
+        /// </summary>
+        /// <param name="wi"></param>
+        /// <returns></returns>
+        public static ChildWindowInfo GetChildWindowInfo(this WindowInfo wi, IntPtr? hwnd, string windowName, string className)
+        {
+            List<ChildWindowInfo> windowInfoList = wi.GetAllChildWindowList();
+
+            if (hwnd != null && hwnd != (IntPtr)0)
+            {
+                if (windowInfoList.FindIndex(o => o.Hwnd == hwnd) != -1)
+                    return windowInfoList.Find(o => o.Hwnd == hwnd);
+            }
+            else if (windowName != null)
+            {
+                if (windowInfoList.FindIndex(o => o.WindowName.Contains(windowName)) != -1)
+                    return windowInfoList.Find(o => o.WindowName.Contains(windowName));
+            }
+            else if (className != null)
+            {
+                if (windowInfoList.FindIndex(o => o.ClassName.Contains(className)) != -1)
+                    return windowInfoList.Find(o => o.ClassName.Contains(className));
+            }
+            return new ChildWindowInfo();
+        }
+
+        /// <summary>
+        /// 获取该子窗口的文本内容
+        /// </summary>
+        /// <param name="cwi"></param>
+        public static string GetContent(this ChildWindowInfo cwi)
+        {
+            StringBuilder sb = new StringBuilder(4048);
+            SendMessage(cwi.Hwnd, WM_GETTEXT, sb.Capacity, sb);
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 设置该子窗口的文本内容
+        /// </summary>
+        /// <param name="cwi"></param>
+        /// <param name="content"></param>
+        public static void SetContent(this ChildWindowInfo cwi, string content)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(content);
+            SendMessage(cwi.Hwnd, WM_SETTEXT, 0, sb);
+        }
+
+        /// <summary>
+        /// 模拟鼠标点击此窗口
+        /// </summary>
+        /// <param name="cwi"></param>
+        public static void Click(this ChildWindowInfo cwi)
+        {
+            SetForegroundWindow(cwi.Hwnd);
+            SendMessage(cwi.Hwnd, WM_LBUTTONDOWN, 0, null);
+            SendMessage(cwi.Hwnd, WM_LBUTTONUP, 0, null);
+        }
 
 
+        /// <summary>
+        /// 回调函数
+        /// </summary>
+        /// <param name="hwnd"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        public delegate bool CallBack(IntPtr hwnd, int lParam);
         /// <summary>
         /// 查找窗口句柄
         /// </summary>
@@ -63,7 +209,7 @@ namespace XtuHandleUitls
         /// </summary>
         /// <param name="hwnd">窗口句柄</param>
         /// <returns></returns>
-        [DllImport("user32.DLL", EntryPoint = "SetForegroundWindow")]
+        [DllImport("user32.dll", EntryPoint = "SetForegroundWindow")]
         public static extern bool SetForegroundWindow(IntPtr hwnd);
 
         /// <summary>
@@ -75,8 +221,7 @@ namespace XtuHandleUitls
         /// <returns></returns>
 
         [DllImport("user32.dll", EntryPoint = "EnumChildWindows")]
-        public static extern int EnumChildWindows(IntPtr hWndParent, CallBack lpCallBack, int lParam);
-        public delegate bool CallBack(int hwnd, int lParam);
+        public static extern int EnumChildWindows(IntPtr hWndParent, CallBack lCallBack, int lParam);
 
         /// <summary>
         /// 发送消息
@@ -88,6 +233,33 @@ namespace XtuHandleUitls
         /// <returns></returns>
         [DllImport("User32.dll", EntryPoint = "SendMessage")]
         public static extern int SendMessage(IntPtr hwnd, int msg, int wParam, StringBuilder lParam);
+
+        /// <summary>
+        /// 获取桌面句柄
+        /// </summary>
+        /// <returns></returns>
+
+        [DllImport("user32.dll", EntryPoint = "GetDesktopWindow")]
+        public static extern IntPtr GetDesktopWindow();
+
+        /// <summary>
+        /// 枚举所有窗口
+        /// </summary>
+        /// <param name="lCallBack"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", EntryPoint = "EnumWindows")]
+        public static extern bool EnumWindows(CallBack lCallBack, int lParam);
+
+        /// <summary>
+        /// 获取窗口类名
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="text"></param>
+        /// <param name="maxLength"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", EntryPoint = "GetClassName")]
+        private static extern int GetClassName(IntPtr hWnd, StringBuilder text, int maxLength);
 
     }
 }
